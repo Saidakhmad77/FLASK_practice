9e7f27dbd3267c25d2ed03d0bc41f06c0cfd4e39
Saidakhmad77 <lionuz669@gmail.com>
2024-09-04 16:02:03 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/home')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/homepage')
+def get_homepage():
+    return render_template('hello.html')
+
+@app.route('/hello')
+def hello():
+    return '<p>Hello, World</p>'
+
+@app.route('/goodbye')
+def goodbye():
+    x = create_title()
+    return x
+
+def create_title():
+    return '<p>Title page</p>'
